964f088a583523b21b280cb6e0f840a666452330
aclec <anicetprodhomme@gmail.com>
2024-11-25 12:17:05 +0100

    prettier

--- example/src/components/navigation/TabBarIcon.tsx
+++ example/src/components/navigation/TabBarIcon.tsx
@@ -2,6 +2,9 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { type IconProps } from '@expo/vector-icons/build/createIconSet';
 import { type ComponentProps } from 'react';
 
-export function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {
+export function TabBarIcon({
+  style,
+  ...rest
+}: IconProps<ComponentProps<typeof Ionicons>['name']>) {
   return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
 }
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = async (env, argv) => {
         dangerouslyAddModulePathsToTranspile: ['expo-zebra-scanner'],
       },
     },
-    argv
+    argv,
   );
   config.resolve.modules = [
     path.resolve(__dirname, './node_modules'),
